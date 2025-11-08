3a4cc616fbe5a272fdf8a693725d3761aa2ee592
dcelysia <6873058@qq.com>
2025-11-08 23:32:25 +0800

    fix: 1. 修复了网页版中文消失的问题

--- composeApp/src/commonMain/kotlin/com/dcelysia/changli_planet_admin/App.kt
+++ composeApp/src/commonMain/kotlin/com/dcelysia/changli_planet_admin/App.kt
@@ -1,32 +1,28 @@
 package com.dcelysia.changli_planet_admin
 
 import androidx.compose.runtime.*
-import coil3.ImageLoader
-import coil3.compose.setSingletonImageLoaderFactory
 import com.dcelysia.changli_planet_admin.core.storage.TokenManager
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import com.dcelysia.changli_planet_admin.feature.auth.presentation.ui.LoginScreen
 import com.dcelysia.changli_planet_admin.ui.MainScreen
-import com.dcelysia.changli_planet_admin.ui.theme.AppTheme
 
 @Composable
 @Preview
 fun App() {
     var isLoggedIn by remember { mutableStateOf(TokenManager.isLoggedIn()) }
-    AppTheme {
-        if (isLoggedIn) {
-            MainScreen(
-                onLogout = {
-                    TokenManager.clearAll()
-                    isLoggedIn = false
-                }
-            )
-        } else {
-            LoginScreen(
-                onLoginSuccess = {
-                    isLoggedIn = true
-                }
-            )
-        }
+
+    if (isLoggedIn) {
+        MainScreen(
+            onLogout = {
+                TokenManager.clearAll()
+                isLoggedIn = false
+            }
+        )
+    } else {
+        LoginScreen(
+            onLoginSuccess = {
+                isLoggedIn = true
+            }
+        )
     }
 }
